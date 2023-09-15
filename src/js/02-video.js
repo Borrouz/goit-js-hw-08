dd313119904df93002d5a33306edb923d5a8f5e6
Borrouz <brovkov.ievgen@gmail.com>
2023-09-15 17:46:48 +0300

    task 2 reload

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,17 +2,17 @@ import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-
+const currentTime = 'videoplayer-current-time';
 const locStorSet = function(e) {
     console.log(e.seconds);
     const startSec = Number(e.seconds);
-  localStorage.setItem('videoplayer-current-time', startSec)
+  localStorage.setItem(currentTime, startSec)
    
 }
 const playerOn = player.on('timeupdate', throttle(locStorSet, 1000));
 
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
+player.setCurrentTime(localStorage.getItem(currentTime)).then(function(seconds) {
     // seconds = the actual time that the player seeked to
 }).catch(function(error) {
     switch (error.name) {
